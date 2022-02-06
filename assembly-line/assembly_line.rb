40d35f22be605692044f6fa6a07a3c9f4e015ce0
Wiewior <sysk472@gmail.com>
2022-02-06 15:20:50 +0100

    2nd iteration of assembly line

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,26 +1,31 @@
 class AssemblyLine
-  
   NUMBER_OF_CARS_PER_HOUR = 221
+  
   def initialize(speed)
+    raise ArgumentError, 'speed must be between 1 and 10' unless speed.between?(1, 10)
+    
     @speed = speed
   end
 
   def production_rate_per_hour
-    rate = case @speed
-           when 1..4
-             1
-           when 5..8
-             0.9
-           when 9
-             0.8
-           when 10
-             0.77
-           end
-
-    NUMBER_OF_CARS_PER_HOUR * @speed * rate
+    NUMBER_OF_CARS_PER_HOUR * @speed * success_rate
   end
 
   def working_items_per_minute
     (production_rate_per_hour / 60).to_i
   end
+  
+  def success_rate
+    case @speed
+    when 1..4
+      1
+    when 5..8
+      0.9
+    when 9
+      0.8
+    when 10
+      0.77
+    end
+  end
+
 end
