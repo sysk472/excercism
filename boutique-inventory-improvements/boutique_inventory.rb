9230dc53bcf96c6849bd59ce3bc589ebaf3971ac
Wiewior <sysk472@gmail.com>
2022-02-11 16:00:19 +0100

    Boutique inventory improvement

---
+++ boutique-inventory-improvements/boutique_inventory.rb
@@ -0,0 +1,21 @@
+require 'ostruct'
+
+class BoutiqueInventory
+  attr_reader :items
+
+  def initialize(items)
+    @items = items.map do |item|
+      OpenStruct.new(item)
+    end
+  end
+
+  def item_names
+    items.map { |item| item.name }.sort
+  end
+
+  def total_stock
+    items.sum do |item|
+      item.quantity_by_size.values.sum
+    end
+  end
+end
